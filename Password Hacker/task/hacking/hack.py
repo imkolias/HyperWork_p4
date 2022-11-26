a003cc0dc7052f9e13245600cca29c26163e8b0c
Nik Jakimovec <imkolias@gmail.com>
2022-11-26 12:02:07 +0300

    little polishing

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -17,23 +17,22 @@ with socket.socket() as cli_socket:
     cli_socket.connect((str_arg.ip_address, int(str_arg.port)))
     i = 0
     n = 1
-    while n < 8:
-        for first in itertools.product(psw_list, repeat=n):
-            if i > len(psw_list) ** n: break
+    while chr_count < 8:
+        for first in itertools.product(psw_list, repeat=chr_count):
+            if i > len(psw_list) ** chr_count: break
             cur_psw = "".join(first)
             cli_socket.send(cur_psw.encode())
             recv_data = cli_socket.recv(1024).decode()
             if recv_data == "Connection success!":
-                # print("Connection success!")
                 print(cur_psw)
-                n = 999
+                chr_count = 999
                 break
             elif recv_data == "Too many attempts":
                 print("Too many attempts")
+                chr_count = 999
                 break
-                n = 999
             i += 1
-        n += 1
+        chr_count += 1
 
 
 
